a0f339363735d8fd8107357dec2d45152790f6e3
Christian Griset <christian.griset@clever.com>
2015-09-30 11:24:43 -0700

    Revert "reduce to a single env var"

--- README.md
+++ README.md
@@ -2,37 +2,8 @@
 
 [Godoc](http://godoc.org/github.com/Clever/discovery-go)
 
-## import
+### import
 
 ```go
 import "gopkg.in/Clever/discovery-go.v1"
-```
-
-## Motivation
-
-This library was designed to have a simple way to expose dependencies to a service in an extendable way that allows services to expose multiple interfaces.
-
-## Description
-
-Discovery Go is a standardized library for obtaining a URL to another service given the name of that service and the interface you are connecting to.
-This library is meant to be used in an environment where the service dependencies needed have been provided in environment variables.
-The URL for a service is expected to be in the form `SERVICE_{ SERVICE_NAME }_{ INTERFACE }_URL` in an environment variable.
-
-## Example
-
-Let's imagine a service has a dependency on influxdb and needs to send data to the [protobuf TCP interface](https://github.com/Clever/influxdb-service/blob/master/launch/influxdb-service.yml#L20-L23).
-This service can obtain the proper URL to access this interface by specifying `influxdb-service` and `protobuf` in their call.
-
-```go
-import discovery "gopkg.in/Clever/discovery-go.v1"
-
-var influxDbURL string
-
-func init() {
-    var err error
-    influxDbURL, err = discovery.URLString("influxdb-service", "protobuf")
-    if err != nil {
-        log.Fatalf("Failed to obtain URL for influxdb, err: %s", err)
-    }
-}
 ```
--- discovery.go
+++ discovery.go
@@ -1,42 +1,78 @@
 package discovery
 
 import (
+	"errors"
 	"fmt"
 	"net/url"
 	"os"
 	"strings"
+
+	kv "gopkg.in/clever/kayvee-go.v2"
 )
+
+// m is a convenience type for using kv.
+type m map[string]interface{}
 
 const (
-	templateVar = "SERVICE_%s_%s_URL"
+	templateVar = "SERVICE_%s_%s_%%s"
 )
 
-// URLString finds the specified URL for a service based off of the service's name and which
-// interface you are accessing. Values are found in environment variables fitting the scheme:
-// SERVICE_{SERVICE NAME}_{INTERFACE NAME}_URL.
-func URLString(service, name string) (string, error) {
-	// build string
-	envKey := fmt.Sprintf(templateVar, service, name)
-
-	// standardize to uppercase & underscores
-	envKey = strings.Replace(strings.ToUpper(envKey), "-", "_", -1)
-
-	val := os.Getenv(envKey)
+func getVar(envVar string) (string, error) {
+	envVar = strings.ToUpper(envVar)
+	envVar = strings.Replace(envVar, "-", "_", -1)
+	val := os.Getenv(envVar)
 	if val == "" {
-		return "", fmt.Errorf("Missing env var, %s", envKey)
+		return "", errors.New(kv.FormatLog("discovery-go", kv.Error, "missing env var", m{
+			"var": envVar,
+		}))
 	}
 	return val, nil
 }
 
 // URL finds the specified URL for a service based off of the service's name and which
 // interface you are accessing. Values are found in environment variables fitting the scheme:
-// SERVICE_{SERVICE NAME}_{INTERFACE NAME}_URL.
-func URL(service, name string) (*url.URL, error) {
-	// get string
-	s, err := URLString(service, name)
+// SERVICE_{SERVICE NAME}_{INTERFACE NAME}_{PROTO,HOST,PORT}.
+func URL(service, name string) (string, error) {
+	proto, err := Proto(service, name)
 	if err != nil {
-		return nil, err
+		return "", err
+	}
+	host, err := Host(service, name)
+	if err != nil {
+		return "", err
+	}
+	port, err := Port(service, name)
+	if err != nil {
+		return "", err
 	}
 
-	return url.Parse(s)
+	u := url.URL{
+		Scheme: proto,
+		Host:   fmt.Sprintf("%s:%s", host, port),
+	}
+	return u.String(), nil
+}
+
+// Proto finds the specified protocol for a service based off of the service's name and which
+// interface you are accessing. Values are found in environment variables fitting the scheme:
+// SERVICE_{SERVICE NAME}_{INTERFACE NAME}_PROTO.
+func Proto(service, name string) (string, error) {
+	template := fmt.Sprintf(templateVar, service, name)
+	return getVar(fmt.Sprintf(template, "PROTO"))
+}
+
+// Host finds the specified host for a service based off of the service's name and which
+// interface you are accessing. Values are found in environment variables fitting the scheme:
+// SERVICE_{SERVICE NAME}_{INTERFACE NAME}_HOST.
+func Host(service, name string) (string, error) {
+	template := fmt.Sprintf(templateVar, service, name)
+	return getVar(fmt.Sprintf(template, "HOST"))
+}
+
+// Port finds the specified port for a service based off of the service's name and which
+// interface you are accessing. Values are found in environment variables fitting the scheme:
+// SERVICE_{SERVICE NAME}_{INTERFACE NAME}_PORT.
+func Port(service, name string) (string, error) {
+	template := fmt.Sprintf(templateVar, service, name)
+	return getVar(fmt.Sprintf(template, "PORT"))
 }
